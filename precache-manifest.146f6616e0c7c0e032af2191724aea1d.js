4ec73209a4e9e51f112208b13d0249d735a0a684
waseemahmad01 <wa031522@gmail.com>
2020-08-19 12:44:48 +0500

    Updates

---
+++ precache-manifest.146f6616e0c7c0e032af2191724aea1d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "441099c12f6bb939d5232f0e52ec0802",
+    "url": "/shopping-cart/index.html"
+  },
+  {
+    "revision": "e78a6333fbd5ef4d2cfd",
+    "url": "/shopping-cart/static/css/main.5acf90e7.chunk.css"
+  },
+  {
+    "revision": "4ec97f496ee558dc0cc3",
+    "url": "/shopping-cart/static/js/2.6b823506.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/shopping-cart/static/js/2.6b823506.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e78a6333fbd5ef4d2cfd",
+    "url": "/shopping-cart/static/js/main.0c36e439.chunk.js"
+  },
+  {
+    "revision": "b6d8735b3efcef93d51d",
+    "url": "/shopping-cart/static/js/runtime-main.31b3e1ca.js"
+  }
+]);
